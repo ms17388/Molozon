a25179ef48d11297ba2062cdcb888bb247bbb3b3
ms17388 <245794767+ms17388@users.noreply.github.com>
2026-02-10 18:10:32 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const toggle = document.getElementById("menu-toggle");
 const navLinks = document.querySelector(".nav-links");
 
-toggle.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
+if (toggle) {
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
